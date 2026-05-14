ce7621fecab0b440b52cf2be4990e631dbacfcdc
Fabricio Sartor <frssartor@gmail.com>
2026-05-14 20:26:59 -0300

    Agregando funciones basicas

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import './config/env.js'
+import connectDB from './config/db.js'
+import userRouters from './routers/user.routes.js'
+
+const app = express()
+
+app.use(express.json())
+connectDB()
+
+app.use(userRoutes)
+
+app.listen(process.env.Port, () => (
+    console.log('Servidor corriendo en puerto ${process.env.Port}')
+))
